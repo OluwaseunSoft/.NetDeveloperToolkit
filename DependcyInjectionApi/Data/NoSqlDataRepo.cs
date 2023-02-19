42dc8de6704e606d539fcf3bcc1b73097c31af70
Oluwaseun Akinwale <oluwaseunazure@yahoo.com>
2023-02-19 21:40:07 +0100

    build/run

--- DependcyInjectionApi/Data/NoSqlDataRepo.cs
+++ DependcyInjectionApi/Data/NoSqlDataRepo.cs
@@ -4,20 +4,27 @@ namespace DiApi.Data
 {
     public class NoSqlDataRepo : IDataRepo
     {
-        private readonly IDataService _dataService;
+        private readonly IServiceScopeFactory _scopeFactory;
 
-        public NoSqlDataRepo(IDataService dataService)
+        //private readonly IDataService _dataService;
+
+        public NoSqlDataRepo(IServiceScopeFactory scopeFactory)
         {
-            _dataService = dataService;
+            //_dataService = dataService;
+            _scopeFactory = scopeFactory;
         }
 
         public string ReturnData()
         {
             Console.ForegroundColor = ConsoleColor.Red;
             System.Console.WriteLine("--> Getting Data From NoSql DB");
-            _dataService.GetProductData("https://something.com/api");
-            Console.ResetColor();
-            return ("NoSql data from Db");
+            using (var scope = _scopeFactory.CreateScope())
+            {
+                var dataService = scope.ServiceProvider.GetRequiredService<IDataService>();
+                dataService.GetProductData("https://something.com/api");
+                Console.ResetColor();
+                return ("NoSql data from Db");
+            }
         }
     }
 }
