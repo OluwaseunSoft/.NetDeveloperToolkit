7af1e2b928a7dc600bd46ceabab2901b65ea3c05
Oluwaseun Akinwale <oluwaseunazure@yahoo.com>
2023-02-19 22:21:52 +0100

    di commit

--- DependcyInjectionApi/Program.cs
+++ DependcyInjectionApi/Program.cs
@@ -1,21 +1,31 @@
 using DiApi.Data;
 using DiApi.DataServices;
+using DiApi.Middleware;
+using DiApi.Utility;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddScoped<IDataRepo, NoSqlDataRepo>();
+builder.Services.AddSingleton<IDataRepo, NoSqlDataRepo>();
 builder.Services.AddScoped<IDataService, HttpDataService>();
+
+builder.Services.AddTransient<IOperationTransient, Operation>();
+builder.Services.AddScoped<IOperationScoped, Operation>();
+builder.Services.AddSingleton<IOperationSingleton, Operation>();
 
 var app = builder.Build();
 
+app.UseCustomMiddleware();
+
 app.UseHttpsRedirection();
 
-app.MapGet("/getdata", (IDataRepo repo) =>
+app.MapGet("/getdata", (IOperationTransient transient, IOperationScoped scoped, IOperationSingleton singleton) =>
 {
     //var repo = new SqlDataRepo();
     //var repo = new NoSqlDataRepo();
 
-    repo.ReturnData();
+    //repo.ReturnData();
+
+    System.Console.WriteLine($"Endpoint: TransientId: {transient.OperationId} ScopedId: {scoped.OperationId} Singleton {singleton.OperationId}");
 
     return Results.Ok();
 });
