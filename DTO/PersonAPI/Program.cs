a1e178b41740bb35a109e99e163e3b8f7263b2c9
Oluwaseun Akinwale <oluwaseunazure@yahoo.com>
2023-03-06 04:48:45 +0100

    init

--- DTO/PersonAPI/Program.cs
+++ DTO/PersonAPI/Program.cs
@@ -35,17 +35,31 @@ app.MapGet("api/v1/people/{id}", async (AppDbContext context, int id) =>
 
     if (person == null)
         return Results.NotFound();
-        
+
     return Results.Ok(person);
 });
 
 app.MapPost("api/v1/people", async (AppDbContext context, Person person) =>
 {
     await context.People.AddAsync(person);
-    await context.SaveChangesAsync();        
+    await context.SaveChangesAsync();
     return Results.Created($"/api/v1/people/{person.Id}", person);
 });
 
+app.MapPut("api/v1/people/{id}", async (AppDbContext context, int id, Person person) =>
+{
+    var personModel = await context.People.FindAsync(id);
+
+    if (personModel == null)
+        return Results.NotFound();
+
+    personModel.FullName = person.FullName;
+    personModel.Telephone = person.Telephone;
+    personModel.DoB = person.DoB;
+
+    await context.SaveChangesAsync();
+    return Results.NoContent();
+});
 
 app.UseHttpsRedirection();
 
