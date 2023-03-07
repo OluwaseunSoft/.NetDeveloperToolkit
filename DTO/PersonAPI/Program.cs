48cb1609b6b50ffd63e78c957eec7a48fd7ce4e9
Oluwaseun Akinwale <oluwaseunazure@yahoo.com>
2023-03-07 12:24:41 +0100

    init1

--- DTO/PersonAPI/Program.cs
+++ DTO/PersonAPI/Program.cs
@@ -11,6 +11,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 //
+//
 
 builder.Services.AddDbContext<AppDbContext>(opt => opt
 .UseSqlServer(builder.Configuration.GetConnectionString("SqlDbConnection")
